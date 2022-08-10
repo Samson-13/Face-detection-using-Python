1663ba98ecd38d0fd987bd308ef0227105280fc8
M Samson Badding <85184417+Samson-13@users.noreply.github.com>
2022-08-10 21:39:41 +0530

    Add files via upload

---
+++ face detect image.py
@@ -0,0 +1,23 @@
+import cv2 as cv
+from cv2 import imread
+
+# The image file
+img = cv.imread('pic1.jpg')
+
+# using the haarcascade frontalface default.xml download from the link below
+# https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
+detect = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Converting the image to Grayscale
+bnw = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+face_coor = detect.detectMultiScale(bnw)
+
+# The coordinates and detection
+for (x, y, w, h) in face_coor:
+    rect = cv.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2)
+
+# Output
+cv.imshow('The image', rect)
+
+cv.waitKey()
